4c5a5ead49b82d751cf50dca9bddad4ac6886deb
Plub15 <102942511+Plub15@users.noreply.github.com>
2026-09-05 23:42:44 +0700

    Add rule where robot cannot pickup where there is no target

--- rules.py
+++ rules.py
@@ -19,3 +19,18 @@ def can_move_forward(robot, next_x, next_y):
         robot.id,
     )
     return False
+
+def can_pick_up(robot):
+    """Return whether a robot may pick up a target"""
+    if robot.world is None:
+        return False
+
+    current_block = robot.world.get_block(robot.x, robot.y)
+    if len(current_block.targets) == 0:
+        logger.warning(
+            "Robot %s tried to pick up a target where there is none",
+            robot.id,
+        )
+        return False
+
+    return True
